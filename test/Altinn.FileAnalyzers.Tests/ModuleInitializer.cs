3e16c9fa20dfd6f276279a4d8c41604e8895aba3
Martin Othamar <martin.othamar@digdir.no>
2025-06-27 19:11:25 +0200

    snapshot

--- test/Altinn.FileAnalyzers.Tests/ModuleInitializer.cs
+++ test/Altinn.FileAnalyzers.Tests/ModuleInitializer.cs
@@ -2,17 +2,11 @@ using System.Runtime.CompilerServices;
 
 namespace Altinn.Codelists.Tests;
 
-public static class ModuleInitializer
+internal static class ModuleInitializer
 {
     [ModuleInitializer]
     public static void Initialize()
     {
-        // Auto-accept changes to snapshots when not running in CI
-        // This allows snapshots to be automatically updated during local development
-        // but ensures they are verified (not auto-updated) in CI environments
-        if (Environment.GetEnvironmentVariable("CI") != "true")
-        {
-            VerifierSettings.AutoVerify();
-        }
+        VerifierSettings.AutoVerify(includeBuildServer: false);
     }
 }
